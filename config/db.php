ff8d8e94962e021e9794c45a919af0d128cd9036
shiyan <1316796206@qq.com>
2019-06-26 17:53:02 +0800

    legend templete

---
+++ config/db.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=127.0.0.1;dbname=admin',
+    'username' => 'root',
+    'password' => 'root',
+    'charset' => 'utf8',
+    'tablePrefix'=>'cy_'
+];
