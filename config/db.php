3ddd2fbde226a96e6dc4e57fa42f5850c4571a08
shiyan <1316796206@qq.com>
2019-06-26 18:02:13 +0800

    legend admn

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=127.0.0.1;dbname=admin',
+    'dsn' => 'mysql:host=127.0.0.1;dbname=legend',
     'username' => 'root',
     'password' => 'root',
     'charset' => 'utf8',
