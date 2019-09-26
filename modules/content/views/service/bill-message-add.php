37f8ab53511f1b1cc18cdc3d8470e123e2d7e742
shiyan <1316796206@qq.com>
2019-09-26 21:02:19 +0800

    bill message

--- modules/content/views/service/bill-message-add.php
+++ modules/content/views/service/bill-message-add.php
@@ -21,11 +21,8 @@
                         }
                         ?>
                     </select>
-                </div>
-            </div>
-            <div class="control-group">
-                <label for="modulename" class="control-label"><span style="color:red">* </span>单据来源</label>
-                <div class="controls">
+
+                    <label style="display: inline;margin-left: 180px;margin-right: 20px"><span style="color:red">* </span>单据来源</label>
                     <select name="billSource" >
                         <option value="0">请选择</option>
                         <?php
@@ -36,6 +33,32 @@
                         ?>
                     </select>
                 </div>
+            </div>
+            <div class="control-group">
+                <label  for="modulename" class="control-label"><span style="color:red">* </span>一级分类</label>
+                <div class="controls">
+                    <select name="billGame">
+                        <option value="0">请选择</option>
+                        <?php
+                        foreach($billQuesParent as $k => $v){ ?>
+                            <option value='<?php echo $v['id']?>' <?php if(isset($bill['quesParent']) && $bill['quesParent'] == $v['id']) echo 'selected';?>><?php echo $v['name']?></option>";
+                            <?php
+                        }
+                        ?>
+                    </select>
+
+                    <label style="display: inline;margin-left: 180px;margin-right: 20px"><span style="color:red">* </span>二级分类</label>
+                    <select name="billGame" >
+                        <option value="0">请选择</option>
+                        <?php
+                        foreach($billQuesChild as $k => $v){ ?>
+                            <option value='<?php echo $v['id']?>' <?php if(isset($bill['quesChild']) && $bill['quesChild'] == $v['id']) echo 'selected';?>><?php echo $v['name']?></option>";
+                            <?php
+                        }
+                        ?>
+                    </select>
+                </div>
+
             </div>
             <div class="control-group">
                 <label for="modulename" class="control-label"><span style="color:red">* </span>游戏所属</label>
@@ -49,46 +72,8 @@
                         }
                         ?>
                     </select>
-                </div>
-            </div>
-            <div class="control-group">
-                <label for="modulename" class="control-label"><span style="color:red">* </span>一级分类</label>
-                <div class="controls">
-                    <select name="billGame">
-                        <option value="0">请选择</option>
-                        <?php
-                        foreach($billQuesParent as $k => $v){ ?>
-                            <option value='<?php echo $v['id']?>' <?php if(isset($bill['quesParent']) && $bill['quesParent'] == $v['id']) echo 'selected';?>><?php echo $v['name']?></option>";
-                            <?php
-                        }
-                        ?>
-                    </select>
-                </div>
-            </div>
-            <div class="control-group">
-                <label for="modulename" class="control-label"><span style="color:red">* </span>二级分类</label>
-                <div class="controls">
-                    <select name="billGame" >
-                        <option value="0">请选择</option>
-                        <?php
-                        foreach($billQuesChild as $k => $v){ ?>
-                            <option value='<?php echo $v['id']?>' <?php if(isset($bill['quesChild']) && $bill['quesChild'] == $v['id']) echo 'selected';?>><?php echo $v['name']?></option>";
-                            <?php
-                        }
-                        ?>
-                    </select>
-                </div>
-            </div>
-            <div class="control-group">
-                <label for="modulename" class="control-label">游戏账号</label>
-                <div class="controls">
-                    <input   type="text" id="account" name="account"  value="<?php echo isset($bill['account'])?$bill['account']:''?>" autocomplete="off"/>
-                </div>
-            </div>
 
-            <div class="control-group">
-                <label for="modulename" class="control-label">VIP等级</label>
-                <div class="controls">
+                    <label style="display: inline;margin-left: 190px;margin-right: 26px">VIP等级</label>
                     <select name="vipLevels" >
                         <option value="0">请选择</option>
                         <?php
@@ -101,57 +86,49 @@
                 </div>
             </div>
             <div class="control-group">
-                <label for="modulename" class="control-label">游戏昵称</label>
+                <label for="modulename" class="control-label">游戏账号</label>
                 <div class="controls">
+                    <input   type="text" id="account" name="account"  value="<?php echo isset($bill['account'])?$bill['account']:''?>" autocomplete="off"/>
+
+                    <label style="display: inline;margin-left: 190px;margin-right: 20px">游戏昵称</label>
                     <input size="10" type="text" id="gameName" name="gameName"  value="<?php echo isset($bill['gameName'])?$bill['gameName']:''?>" autocomplete="off"/>
                 </div>
             </div>
+
             <div class="control-group">
                 <label for="modulename" class="control-label"><span style="color:red">* </span>游戏大区</label>
                 <div class="controls">
                     <input  size="10" type="text" id="gameServer" name="gameServer"  value="<?php echo isset($bill['gameServer'])?$bill['gameServer']:''?>" autocomplete="off"/>
+
+                    <label style="display: inline;margin-left: 180px;margin-right: 20px"><span style="color:red">* </span>下载渠道</label>
+                    <input  size="10" type="text" id="download" name="download"  value="<?php echo isset($bill['download'])?$bill['download']:''?>" autocomplete="off"/>
                 </div>
             </div>
             <div class="control-group">
                 <label for="modulename" class="control-label"><span style="color:red">* </span>游戏ID</label>
                 <div class="controls">
                     <input  size="10" type="text" id="gameId" name="gameId"  value="<?php echo isset($bill['gameId'])?$bill['gameId']:''?>" autocomplete="off"/>
-                </div>
-            </div>
-            <div class="control-group">
-                <label for="modulename" class="control-label"><span style="color:red">* </span>下载渠道</label>
-                <div class="controls">
-                    <input  size="10" type="text" id="download" name="download"  value="<?php echo isset($bill['download'])?$bill['download']:''?>" autocomplete="off"/>
+
+                    <label style="display: inline;margin-left: 190px;margin-right: 20px">系统版本</label>
+                    <input  size="10" type="text" id="version" name="version"  value="<?php echo isset($bill['version'])?$bill['version']:''?>" autocomplete="off"/>
                 </div>
             </div>
             <div class="control-group">
                 <label for="modulename" class="control-label">设备型号</label>
                 <div class="controls">
                     <input size="10" type="text" id="device" name="device"  value="<?php echo isset($bill['device'])?$bill['device']:''?>" autocomplete="off"/>
-                </div>
-            </div>
-            <div class="control-group">
-                <label for="modulename" class="control-label">系统版本</label>
-                <div class="controls">
-                    <input  size="10" type="text" id="version" name="version"  value="<?php echo isset($bill['version'])?$bill['version']:''?>" autocomplete="off"/>
+
+                    <label style="display: inline;margin-left: 190px;margin-right: 20px">联系邮箱</label>
+                    <input  size="10" type="text" id="email" name="email"  value="<?php echo isset($bill['email'])?$bill['email']:''?>" autocomplete="off"/>
                 </div>
             </div>
             <div class="control-group">
                 <label for="modulename" class="control-label">联系电话</label>
                 <div class="controls">
                     <input  size="10" type="text" id="phone" name="phone"  value="<?php echo isset($bill['phone'])?$bill['phone']:''?>" autocomplete="off"/>
-                </div>
-            </div>
-            <div class="control-group">
-                <label for="modulename" class="control-label">联系QQ</label>
-                <div class="controls">
+
+                    <label style="display: inline;margin-left: 190px;margin-right: 25px">联系QQ</label>
                     <input  size="10" type="text" id="qq" name="qq"  value="<?php echo isset($bill['qq'])?$bill['qq']:''?>" autocomplete="off"/>
-                </div>
-            </div>
-            <div class="control-group">
-                <label for="modulename" class="control-label">联系邮箱</label>
-                <div class="controls">
-                    <input  size="10" type="text" id="email" name="email"  value="<?php echo isset($bill['email'])?$bill['email']:''?>" autocomplete="off"/>
                 </div>
             </div>
             <div class="control-group">
@@ -160,18 +137,19 @@
                     <input  size="10" type="text" id="imageFile" name="imageFile"  value="<?php echo isset($bill['email'])?$bill['email']:''?>" autocomplete="off"/>
                 </div>
             </div>
+            <br/>
             <div class="control-group">
                 <label for="modulename" class="control-label"><span style="color:red">* </span>详细描述</label>
                 <div class="controls">
-                    <textarea  id="detail" name="detail"><?php echo isset($bill['detail'])?$bill['detail']:''?></textarea>
+                    <textarea style="width: 702px;height: 100px" id="detail" name="detail"><?php echo isset($bill['detail'])?$bill['detail']:''?></textarea>
                 </div>
             </div>
             <div class="control-group">
                 <label for="modulename" class="control-label"><span style="color:red">* </span>处理结果</label>
                 <div class="controls">
-                    <textarea  id="result" name="result"><?php echo isset($bill['result'])?$bill['result']:''?></textarea>
+                    <textarea style="width: 702px;height: 100px"  id="result" name="result"><?php echo isset($bill['result'])?$bill['result']:''?></textarea>
                 </div>
-            </div>
+            </div><br/>
             <div class="control-group">
                 <div class="controls">
                     <input type="submit" class="btn btn-primary" value="提交">
