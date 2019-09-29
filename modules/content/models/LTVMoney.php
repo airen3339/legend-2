2b512e6ab515f7dee3e5ae8e06cc0f3f614a674b
shiyan <1316796206@qq.com>
2019-09-29 14:25:35 +0800

    ltv data test

--- modules/content/models/LTVMoney.php
+++ modules/content/models/LTVMoney.php
@@ -17,7 +17,7 @@ class LTVMoney extends ActiveRecord
 //        $today = date('Y-m-d');
         $beginTime = strtotime($today);
         $endTime = $beginTime + 86399;
-        $dates = LTV::find()->asArray("unix_timestamp(date) <= $beginTime")->all();
+        $dates = LTV::find()->where("unix_timestamp(date) <= $beginTime")->asArray()->all();
         foreach($dates as $k => $v){
             $ltvId = $v['id'];
             $deviceMsg = $v['deviceMsg'];
