955fdb03b62b734c5a2085f3cb8e2fc96d8099be
oathYc <1316796206@qq.com>
2019-10-22 10:32:30 +0800

    wx h5 pay

--- modules/content/views/player/h5-pay.php
+++ modules/content/views/player/h5-pay.php
@@ -6,11 +6,18 @@
           content="width=device-width,initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
     <title>雷哥单词</title>
     <link rel="stylesheet" href="/cn/css/register.css?v=1.1">
+    <style>
+        .wxBtn{
+            background: rgba(55, 188, 133, 1);
+            width: 77px;
+            height: 43px;
+            border-radius: 9px;
+            position: relative;
+            left: 35%;
+        }
+    </style>
 </head>
 <body>
-<header>
-
-</header>
 <div class="content">
     <p>订单信息</p>
 
@@ -37,12 +44,25 @@
             <input placeholder=""  value="<?php echo isset($order['yuanbao'])?$order['yuanbao']:''?>"    readonly/>
 
         </p>
+
+        <p>
+            <button class="wxBtn" onclick="getWxpayUrl()">立即支付</button>
+        </p>
+
     </div>
 </div>
 
 <script src="https://cdn.bootcss.com/jquery/3.3.1/jquery.min.js"></script>
 <script type="text/javascript">
-
+    function getWxpayUrl(){
+        $.post('/pay/wx/test2',{},function(e){
+            if(e.code ==1){
+                location.href=e.payUrl
+            }else{
+                alert(e.code);
+            }
+        },'json');
+    }
 </script>
 
 </body>
