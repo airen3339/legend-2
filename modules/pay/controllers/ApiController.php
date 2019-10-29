fe29c7da9e0a27c232b0cca172cca97e3180ae58
oathYc <1316796206@qq.com>
2019-10-29 11:27:06 +0800

    alipay test order

--- modules/pay/controllers/ApiController.php
+++ modules/pay/controllers/ApiController.php
@@ -81,59 +81,15 @@ class ApiController extends Controller
         $model->payType = 1;
         $model->yuanbao = $ratio*$amount+$luckNum;
         $model->save();
-//        通知服务器
-//        self::dataToServer($orderNumber,$productName,$amount,1,$date,$detail);
         $return = self::AliOrder($orderNumber,$productName,$amount,$dateTime,$province,$city,$area,$model->id);
         die(json_encode($return));
     }
-
-    /**
-     * 清逸支付
-     */
-    public function actionQingYiOrder(){
-        $request = \Yii::$app->request;
-        $productName = $request->post('productName','ceshi2');
-        $amount = $request->post('amount',1);
-        $detail = $request->post('detail','');
-        $time = time();
-        $date = date('Y-m-d H:i:s',$time);
-        $orderNumber = 'YCJ'.time();
-        $pay_type = $request->post('pay_type','');//支付类型
-        $ip_add = $request->post('ip_add','');//客户端ip
-//        通知服务器
-//        self::dataToServer($orderNumber,$productName,$amount,2,$date,$detail);
-        //本地数据记录
-        $uid = $request->post('uid');
-        $ratio = $request->post('ratio');//兑换元宝比例
-        $lucknum = $request->post('lucknum');//额外奖励的元宝数
-        $server_id = $request->post('server_id');//游戏服务器id
-
-        $return = self::QingYiOrder($orderNumber,$productName,$amount,$time,$pay_type,$ip_add);
-        die(json_encode($return));
-    }
-    
-    /**
-     * 通知服务端
-     * 支付请求之前
-     * 数据记录
-     * @param $orderNumber  订单号
-     * @param $productName  商品名称
-     * @param $amount  支付金额
-     * @param $type     支付类型  1-支付宝 2-清逸
-     * @param $time     订单时间
-     * @param $detail   订单详情  其他数据
-     */
-    public static function dataToServer($orderNumber,$productName,$amount,$type,$time,$detail){
-        $postData = [];
-        $postData['orderNumber'] = $orderNumber;
-        $postData['productName'] = $productName;
-        $postData['amount'] = $amount;
-        $postData['payType'] = $type==1?'支付宝':'清逸支付';
-        $postData['orderTime'] = $time;
-        $postData['detail'] = $detail;
-        $url = '';//服务器接受地址
-        Methods::post($url,$postData);
-        return true;
+    public function actionTest(){
+        //签名地区参数 省 市 区
+        $province = \Yii::$app->params['province'];
+        $city = \Yii::$app->params['city'];
+        $area = \Yii::$app->params['area'];
+        self::AliOrder('ycj'.time(),'下单测试','0.01','20191029',$province,$city,$area,12);
     }
 
     /**
@@ -161,6 +117,7 @@ class ApiController extends Controller
         $return = Methods::post($url,$postData);
         Methods::varDumpLog('aliPay.txt',$return,'a');
         $return = json_decode($return,true);
+        var_dump($return);die;
         if($return['code'] == 'success'){
             $returnData = json_decode($return['data'],true);
             $payUrl = $returnData['payUrl'];
@@ -178,37 +135,6 @@ class ApiController extends Controller
             $ip = $_SERVER['REMOTE_ADDR'];
         else $ip = "Unknow";
         return $ip;
-    }
-    /**
-     * 清逸支付
-     * @param $orderNumber  订单号
-     * @param $productName   商品名称
-     * @param $amount       支付金额
-     * @param $time         下单时间  格式时间戳
-     */
-    public static function QingYiOrder($orderNumber,$productName,$amount,$time,$pay_type,$ip_add){
-        $mch_number = '';//商户编码
-        $appname = '';//支付的app
-        $key = '';//商户密钥
-        $jump_url = '';//客户端通知地址
-        $asyn_url = '';//异步通知地址 回调地址
-        $goodsname = $productName;//商品名称
-        $totle_amount = $amount*100;
-        //获取签名
-        $sign_info = self::signQingYi($mch_number,$orderNumber,$pay_type,$ip_add,$key);
-        $url = 'http://121.14.17.174:61869/Sys/CodePool/gmsg1869/pay.aspx';
-        $params = 'mch_number='.$mch_number.'&pay_type='.$pay_type.'&totle_amount='.$totle_amount.'&this_date='.$time.'&order_sn'.$orderNumber.'&jump_url='.$jump_url.'&asyn_url='.$asyn_url.'&ip_add='.$ip_add.'&appname='.$appname.'&goodsname='.$goodsname.'&sign_info='.$sign_info;
-        $api = $url.'?'.$params;
-        $result = file_get_contents($api);
-        $result = json_decode($result,'true');
-        if($result['resultCode'] == 200){
-            $payUrl = $result['qrCode'];
-            $qrUrl = $result['qrUrl'];
-            $data =['code'=>1,'payUrl'=>$payUrl,'qrUrl'=>$qrUrl,'msg'=>'支付请求成功'];
-        }else{
-            $data = ['code'=>0,'msg'=>$result['resultMsg']];
-        }
-        return $data;
     }
 
     /**
@@ -230,22 +156,6 @@ class ApiController extends Controller
         return $signStr;
     }
 
-    /**
-     * 清逸支付签名
-     * 签名生成
-     * md5加密 转小写
-     * @param $mch_number  商户编码
-     * @param $order_sn  订单号
-     * @param $pay_type 支付类型代码
-     * @param $ip_add 客户端真实IP
-     * @param $key  商户密钥
-     */
-    public static function signQingYi($mch_number,$order_sn,$pay_type,$ip_add,$key){
-        $str = $mch_number.$order_sn.$pay_type.$ip_add.$key;
-        $signStr = md5($str);
-        $signStr = strtolower($signStr);
-        return $signStr;
-    }
 
     /**
      * 支付宝回调
@@ -290,68 +200,7 @@ class ApiController extends Controller
         }
         die;
     }
-    public function actionNotifyTest(){
 
-        //验证签名
-        $order = \Yii::$app->request->get('order','');
-        $amount = \Yii::$app->request->get('amount',1);
-        $orderData = Recharge::find()->where("orderNumber = '{$order}' and money = '{$amount}'")->asArray()->one();
-//        if($orderData['status'] != 1){//订单未完成
-            Recharge::updateAll(['status'=>1],"orderNumber='{$order}'");//修改订单状态
-            //通知服务器处理后续
-            $postData = ['uid'=>$orderData['roleId'],'pay_money'=>$amount,'ratio'=>$orderData['ratio'],'lucknum'=>$orderData['lucknum'],'server_id'=>$orderData['server_id'],'sign'=>$orderData['sign'],'order_no'=>$order,'ext_info'=>$orderData['extInfo']];
-            $url = '192.168.0.15:8080';
-            $res = Methods::post($url,$postData);
-            var_dump($res);
-//        }
-        die('SUCCESS');
-    }
-    /**
-     * 清逸支付回调
-     * 支付结果通知处理
-     * 清逸
-     * GET方式
-     */
-    public function actionQingYiNotify(){
-        $order_sn = $_GET['order_sn'];//商户订单号
-        $platform_sn = $_GET['platform_sn'];//清逸系统订单号
-        $totle_amount = $_GET['totle_amount'];//支付金额 单位分
-        $status = $_GET['status'];//支付状态 1-成功 其他失败
-        $sign_info = $_GET['sign_info'];//签名 签名方式：MD5(order_sn+platform_sn+商户密钥) 小写
-        //验证签名
-        $result = self::checkQingYiSign($sign_info,$order_sn,$platform_sn);
-        if($result){
-            if($status ==1){//支付成功
-                //通知服务端 处理后续逻辑
-                $postData = ['orderNumber'=>$order_sn,'amount'=>($totle_amount/100),'success'=>1,'platformNumber'=>$platform_sn];
-                $url = '';
-                Methods::post($url,$postData);
-                echo 'ok';
-            }else{
-//                $postData = ['order_sn'=>$order_sn,'success'=>0];
-                echo 'fail';
-            }
-        }else{
-            echo 'fail,sign error';
-        }
-    }
-    /**
-     * 清逸
-     * 签名验证
-     * 回调地址
-     * 签名方式：MD5(order_sn+platform_sn+商户密钥) 小写
-     */
-    public static function checkQingYiSign($sign_info,$order_sn,$platform_sn){
-        $key = '';//商户密钥
-        $str = $order_sn.$platform_sn.$key;
-        $sign = md5($str);
-        $sign = strtolower($sign);
-        if($sign == $sign_info){
-            return true;
-        }else{
-            return false;
-        }
-    }
     /**
      * 支付宝
      * 签名验证
